57c4a844c761856eeee4c067fafaeee61986f55b
yunsuk-jeung <yunsukjeung@snu.ac.kr>
2020-12-07 22:46:04 +0900

    lmexample

---
+++ cmake-build-debug/CMakeFiles/L_M_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L_M_example.dir/main.cpp.o"
+  "L_M_example"
+  "L_M_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L_M_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
